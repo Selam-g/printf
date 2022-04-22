61860b8b3e09abd8a1a1fad0008abdafb5a5bb1b
Ermias Fekadu <ermias.ju@gmail.com>
2022-04-22 17:13:17 +0300

    C project printf

--- test/holberton.h
+++ test/holberton.h
@@ -1,5 +1,5 @@
-#ifndef MAINH
-#define MAINH
+#ifndef HOLBERTONH
+#define HOLBERTONH
 
 #include <stdarg.h>
 int _putchar(char c);
@@ -8,13 +8,6 @@ int print_char(va_list c);
 int print_string(va_list s);
 int print_int(va_list i);
 int print_dec(va_list d);
-int print_rev(va_list r);
-int print_bin(va_list b);
-int print_unsig(va_list u);
-int print_octal(va_list o);
-int print_x(va_list x);
-int print_X(va_list X);
-int print_rot13(va_list R);
 /**
   * struct code_format - Struct format
   *
@@ -23,8 +16,8 @@ int print_rot13(va_list R);
   */
 typedef struct code_format
 {
-  char *sc;
-  int (*f)(va_list);
+	char *sc;
+	int (*f)(va_list);
 } code_f;
 
 #endif /* HOLBERTONH */
